3fbde440b76de9ac509c6cb88632323a1bed5420
Луговских Данил Викторович <lugovskihdanil@yandex.ru>
2023-08-08 15:33:22 +0700

    first iteration

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.List;
+
 public class Main {
+    public Integer secondMax(List<Integer> values) {
+        if (values.size() == 0) return null;
+        if (values.size() == 1) return values.get(0);
+
+        if (values.size() == 2) return Math.min(values.get(0), values.get(1));
+        Integer minimalValue = Math.min(values.get(0), values.get(1));
+
+        values.remove(minimalValue);
+        if (minimalValue > values.get(values.get(values.size() - 1))) {
+            values.add(minimalValue);
+        }
+
+        return secondMax(values);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
 }
